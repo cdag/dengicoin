6f428bb37112566c75ee82b818d302f99539e75e
cdag <cenobius85@gmail.com>
2024-09-04 13:51:42 -0400

    added package-info.java

---
+++ src/main/java/com/youngops/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * This package contains the classes and interfaces for the Dengicoin project.
+ * 
+ * <p>
+ * The main components of this package include:
+ * <ul>
+ *   <li>{@link com.youngops.Block}</li>
+ *   <li>{@link com.youngops.Blockchain}</li>
+ *   <li>{@link com.youngops.DengiCoinChain}</li>
+ *   <li>{@link com.youngops.MerkleTree}</li>
+ *   <li>{@link com.youngops.StringUtil}</li>
+ *   <li>{@link com.youngops.Transaction}</li>
+ *   <li>{@link com.youngops.Wallet}</li>
+ * </ul>
+ * </p>
+ */
+package com.youngops;
